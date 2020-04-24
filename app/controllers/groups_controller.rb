6632c122b37f8d8607e8c3131716127459c5ac50
tomoya <f.t.1998@i.softbank.jp>
2020-04-24 14:33:27 +0900

    commit

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,8 +1,11 @@
 class GroupsController < ApplicationController
 
+  def show
+  end
+
   def index
   end
-  
+
   def new
     @group = Group.new
     @group.users << current_user
@@ -16,10 +19,24 @@ class GroupsController < ApplicationController
       render :new
     end
   end
-  
+
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to group_messages_path(@group), notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
   private
   def group_params
-    params.require(:group).permit(:name, user_ids: [])
+    params.require(:group).permit(:name, user_ids: [] )
   end
 
 end
