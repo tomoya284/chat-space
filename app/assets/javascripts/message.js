5afc09cb1065c27ebae381839dfc7f18b1a70fbd
tomoya <f.t.1998@i.softbank.jp>
2020-04-27 20:23:13 +0900

    commit

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,7 +2,7 @@ $(function(){
   function buildHTML(message){
    if ( message.image ) {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id="${message.id}">
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -21,7 +21,7 @@ $(function(){
      return html;
    } else {
      var html =
-      `<div class="message">
+      `<div class="message" data-message-id="${message.id}"
          <div class="upper-message">
            <div class="upper-message__user-name">
              ${message.user_name}
@@ -39,19 +39,50 @@ $(function(){
      return html;
    };
  }
+ 
+ var reloadMessages = function() {
+  var last_message_id = $('.message:last').data("message-id");
+  $.ajax({
+    url: "api/messages",
+    type: 'get',
+    dataType: 'json',
+    data: {id: last_message_id}
+  })
+  .done(function(messages) {
+    if (messages.length !== 0) {
+      
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      
+      $('.main-chat__messages').append(insertHTML);
+      $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
+    }
+  })
+  .fail(function() {
+    alert('error');
+  });
+};
+if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+   setInterval(reloadMessages, 7000);
+ }
+
+
 $('#new_message').on('submit', function(e){
  e.preventDefault();
  var formData = new FormData(this);
  var url = $(this).attr('action')
  $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
+  url: url,
+  type: "POST",
+  data: formData,
+  dataType: 'json',
+  processData: false,
+  contentType: false
  })
   .done(function(data){
+    console.log(data)
     var html = buildHTML(data);
     $('.main-chat__messages').append(html);
     $('.main-chat__messages').animate({ scrollTop: $('.main-chat__messages')[0].scrollHeight});
@@ -62,6 +93,9 @@ $('#new_message').on('submit', function(e){
   })
   .fail(function() {
     alert("メッセージ送信に失敗しました");
+  
 });
 })
 });
+
+
